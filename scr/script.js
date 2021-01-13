c71a171af9bcc709082264c9cef0be053cc3166a
EstherGruppen <73243792+EstherGruppen@users.noreply.github.com>
2021-01-13 18:32:24 +0100

    added feels like in html and JS

--- scr/script.js
+++ scr/script.js
@@ -29,6 +29,7 @@ function showTemp(response) {
   document.querySelector("#temp").innerHTML = Math.round(response.data.main.temp);
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
+  document.querySelector("#feelslike").innerHTML = Math.round(response.data.main.feels_like);
   document.querySelector("#icon").setAttribute(
   "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
